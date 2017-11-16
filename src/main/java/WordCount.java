7ef35768e50f2e2af86b497c5051a7bd27057180
maddoxximba <gerardyc@hotmail.com.uk>
2017-11-16 14:46:19 +0000

    1st commit

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,5 +1,72 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 
 public class WordCount {
+    
+    
+    public static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+        
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+        
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()){
+                word.set(tokenizer.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+    
+    
+
+    public static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+    
     public static void main(String[] args) throws Exception {
+        
+        Configuration conf = new Configuration();
+        
+        Job job = Job.getInstance(conf, "WordCount");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        //-Dinput=/home/ubuntu/workspace/tp1/formation-bigdata/dataset/wordcount/hamlet.txt -Doutput=/home/ubuntu/workspace/tp1/formation-bigdata/dataset/wordcount/output.txt
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        // FileInputFormat.addInputPath(job, new Path("/home/ubuntu/workspace/tp1/formation-bigdata/dataset/wordcount/hamlet.txt"));
+        // FileOutputFormat.setOutputPath(job, new Path("/home/ubuntu/workspace/tp1/formation-bigdata/dataset/wordcount/output"));
+        
+        job.waitForCompletion(true);
+        
+
+        
     }
 }
