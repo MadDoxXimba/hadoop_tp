54cc2b24761af1a20d4e7df5493ec5c0a44d52ba
maddoxximba <gerardyc@hotmail.com.uk>
2017-11-17 21:59:00 +0000

    exo 3 ok

--- src/main/java/WordCountExo3.java
+++ src/main/java/WordCountExo3.java
@@ -12,58 +12,92 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Counters;
 
 /*Exercice 3*/
+/*
+Il existe des counters déjà implémenté dans hadoop: exemple JobCounter, FileSystem Counter, MapReduce Task Counter
+
+On peut définir nous même nos propres counters
+
+Les counters peuvent être implémenté dans le reduceur ou le mapper
+
+On définira deux counters pour compter le nombre total des mots commencant par la lettre m
+
+*/
+
 public class WordCountExo3 {
     
+    private static enum M_COUNTER {
+        TOTAL
+    }
     
+    /*Modification du mapper pour écrire que les mots commencant par "m"*/
     public static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
         
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
         
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
-            //convertion de la 1ere ligne du fichier en string
+
             String line = value.toString();
-            //convertion de chaque mot du string en token et suppression des mots usuels (ex: a, by, for, etc...)
+           
             StringTokenizer tokenizer = new StringTokenizer(line);
             while (tokenizer.hasMoreTokens()){
-                //selection du prochain mot sur le string
+
                 word.set(tokenizer.nextToken());
-                //distribution des tâches sur différents serveurs du cluster
-                context.write(word, one);
+                //modification sur le mapper
+                
+                if (word.toString().substring(0,1).equals("m")) {
+                    
+                    //Compatage d'un occurence
+                    
+                    context.getCounter(M_COUNTER.TOTAL).increment(1);
+                    context.write(word, one);
+                }
+                
+                //si modification dans le reducer décommenter ici
+                //context.write(word, one);
             }
         }
     }
     
     
-
+    /*Les modifications */
     public static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
         
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            //opération maths entre chaque couple créer par le mapper
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            //ecriture du fichier résultat sur le cluster
-            context.write(key, new IntWritable(sum));
+            
+            //mettre en commentaire le if  si c'est le mapper qui fait le boulot
+            //if (key.toString().substring(0,1).equals("m")) {
+                
+                int sum = 0;
+                for (IntWritable val : values) {
+                    sum += val.get();
+                }
+
+                context.write(key, new IntWritable(sum));
+                
+           // }
         }
     }
     
+    
     public static void main(String[] args) throws Exception {
+        
+        //Paramètres des jobs
         
         Configuration conf = new Configuration();
         
         Job job = Job.getInstance(conf, "WordCount");
+        
         job.setJarByClass(WordCount.class);
         job.setMapperClass(MyMapper.class);
         job.setReducerClass(MyReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+        
         
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
@@ -73,9 +107,11 @@ public class WordCountExo3 {
         FileInputFormat.addInputPath(job, new Path(inputPath));
         FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
+        //Exo 3
+        //il faut attendre que le job soit paramétrer avant d'utiliser le counter
         job.waitForCompletion(true);
-        
-
-        
+        Counters all_counters = job.getCounters();
+        Counter counterM = all_counters.findCounter(M_COUNTER.TOTAL);
+        System.out.println("M_COUNTER_TOTAL: Ceci est un compteur custom: "+ counterM.getValue());
     }
 }
